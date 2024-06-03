7dad36eebbdabfb86aec627471f4e5308991fdae
Zach Wolek <zach.wolek@gmail.com>
2024-06-03 16:01:21 -0500

    feat: adds API requests for image info

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 import { Homepage } from './pages/Homepage';
+import { getArtifacts, getImage, getArtifactInfo } from './api/api';
 
 function App() {
+
+getArtifacts('Texas')
+getArtifactInfo('184186')
+// getImage('6b1edb9c-0f3f-0ee3-47c7-ca25c39ee360')
+
   return (
       <>
-        <header id='header'>Gotta Gogh</header>
-        <Homepage artworks={artworks}/>
+        <header id='header'>Let's Gogh</header>
+        <Homepage />
       </>
   );
 }
--- src/api/api.js
+++ src/api/api.js
@@ -1,13 +1,41 @@
-function getArtifacts(){
+export function getArtifacts(searchValue){
     return fetch(`https://api.artic.edu/api/v1/artworks/search?q=${searchValue}`)
-    .then((response =>{
+    .then(response =>{
         if(!response.ok){
            throw new Error ('Something has gone wrong at a JSON level');
         } else {
-            return response.json()
+            return response.json();
         }
-    }))
+    })
+    .then(data => console.log(data.data))
 }
+
+
+export function getArtifactInfo(artifactId){
+    return fetch(`https://api.artic.edu/api/v1/artworks/${artifactId}?fields=id,title,image_id`)
+    .then(response =>{
+        if(!response.ok){
+           throw new Error ('Something has gone wrong at a JSON level');
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => console.log("ARTIFACT INFO: ", data.data))
+}
+
+
+export function getImage(identifier){
+    return fetch(`https://www.artic.edu/iiif/2/${identifier}/full/843,/0/default.jpg`)
+    .then(response =>{
+        if(!response.ok){
+           throw new Error ('Something has gone wrong at a JSON level');
+        } else {
+            return response.json();
+        }
+    })
+    .then(data => console.log("IMAGE DATA: ", data))
+}
+
 
 //When downloading images, use /full/843,/0/default.jpg parameters.
 
