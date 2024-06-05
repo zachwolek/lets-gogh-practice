780179abbcc6d5133052af2d5bd00ee69e62ec0b
Zach Wolek <zach.wolek@gmail.com>
2024-06-05 09:16:50 -0500

    Feat: Will add a form to the app page

---
+++ src/Form/Form.js
@@ -0,0 +1,12 @@
+import '../Form/Form.css'
+
+export function Form(){
+    return(
+        <form>
+            <input type="text"
+                
+            />
+            <button>Search</button>
+        </form>
+    )
+}
