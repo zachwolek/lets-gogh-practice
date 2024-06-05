6f338c955991fac27fc70f2d50b623c82b9a2ade
Zach Wolek <zach.wolek@gmail.com>
2024-06-05 16:52:25 -0500

    Will create an API request for page load

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,12 +1,39 @@
 import '../Form/Form.css'
+import { useState } from 'react'
 
-export function Form(){
+export function Form({getArtifacts}){
+    const [searchValue, setValue] = useState('')
+
+    function displaySearch(e){
+        e.preventDefault()
+            const newSearch = {
+                id: Date.now(),
+                searchValue
+            }
+            console.log("NEW SEARCH", newSearch)
+            getArtifacts(newSearch)
+            clearInput()
+    }
+
+    function clearInput(){
+        setValue('')
+    }
+
     return(
         <form>
-            <input type="text"
-                
+            <input  type="text"
+                    placeholder='Search Artifacts'
+                    className='search-form'
+                    name='searchValue'
+                    value={searchValue}
+                    onChange={(e) => setValue(e.target.value)}
             />
-            <button>Search</button>
+            <button
+                onClick = {(e) => displaySearch(e.target.value)}
+            >Search</button>
         </form>
     )
 }
+
+//onclick what is this doing
+//It is sending a variable to the search
